8c483dbeaf66fcb1436ff01fce5eeaa78d4ef725
Roger Huang <roger79118@gmail.com>
2023-12-08 20:25:26 +0800

    update main.py

--- displaypackage/preprocessing.py
+++ displaypackage/preprocessing.py
@@ -1,52 +1,8 @@
-# import cv2
-# import numpy as np
-
-# img = cv2.imread("input/testrgb.tif")
-
-# cv2.imshow('oxxostudio', img)
-
-# contrast = 0    # 初始化要調整對比度的數值
-# brightness = 0  # 初始化要調整亮度的數值
-# cv2.imshow('oxxostudio', img)
-
-
-# # 定義調整亮度對比的函式
-# def adjust(i, c, b):
-#     output = i * (c/100 + 1) - c + b    # 轉換公式
-#     output = np.clip(output, 0, 255)
-#     output = np.uint8(output)
-#     cv2.imshow('oxxostudio', output)
-
-# # 定義調整亮度函式
-# def brightness_fn(val):
-#     global img, contrast, brightness
-#     brightness = val - 100
-#     adjust(img, contrast, brightness)
-
-# # 定義調整對比度函式
-# def contrast_fn(val):
-#     global img, contrast, brightness
-#     contrast = val - 100
-#     adjust(img, contrast, brightness)
-
-# cv2.createTrackbar('brightness', 'oxxostudio', 0, 200, brightness_fn)  # 加入亮度調整滑桿
-# cv2.setTrackbarPos('brightness', 'oxxostudio', 100)
-# cv2.createTrackbar('contrast', 'oxxostudio', 0, 200, contrast_fn)      # 加入對比度調整滑桿
-# cv2.setTrackbarPos('contrast', 'oxxostudio', 100)
-
-# keycode = cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-
-
-
-
-
 import numpy as np
 import cv2
 
-class Control:
+
+class Imagedisplay:
     def __init__(self, path) -> None:
         self.path = path
         
@@ -149,7 +105,3 @@ class Control:
         return self.threshold, self.lowerLimits, self.upperLimits
 
 
-#openimg = Control("input/Hum40-MAP2_GLT1_DAPI.tif")
-openimg = Control("input/testrgb.tif")
-threshold = openimg.createpanel()
-print(threshold)
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from displaypackage.preprocessing import Imagedisplay
+
 if __name__ == '__main__':
     print("Update coming soon!")
+    #openimg = Control("input/Hum40-MAP2_GLT1_DAPI.tif")
+    openimg = Imagedisplay("input/testrgb.tif")
+    threshold = openimg.createpanel()
+    print(threshold)
